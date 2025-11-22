3eeddde54db5836c46f19d9aaf5f708f8b13c29b
sean <154936625+brainwurm@users.noreply.github.com>
2025-11-21 21:48:54 -0500

    Integrate ModifyPost component into AddRecipePage

--- src/app/add-recipe/page.tsx
+++ src/app/add-recipe/page.tsx
@@ -1,8 +1,10 @@
+import ModifyPost from "@/components/ModifyPost"
+
 export default function AddRecipePage() {
   return (
-    <div className="min-h-screen bg-linear-to-br from-[#4C1B7A] via-[#3B0270] to-[#2D0157] text-white p-8">
+     <div className="min-h-screen bg-linear-to-br from-[#4C1B7A] via-[#3B0270] to-[#2D0157] text-white p-8">
       <h1 className="text-3xl font-bold mb-6">Add Recipe</h1>
-      {/* Add recipe form here */}
+    <ModifyPost editPost = {false}/>
     </div>
   );
 }
